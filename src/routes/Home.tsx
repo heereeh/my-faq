e5205b5d95ec8ddd200594120cf81fc1a806d1c0
heereeh <rodbee20@gmail.com>
2023-01-01 10:49:16 +0900

    slack 연결

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,24 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { Conversation, User } from "types";
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import React from "react";
+import { User } from "types";
 import FaqFactory from "components/FaqFactory";
-import { authService, dbService } from "fbase";
+import { authService } from "fbase";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@chakra-ui/react";
+import Faqs from "components/Faqs";
 
 const Home = ({user}: {user: User}) => {
-  const [convs, setConvs] = useState<Conversation[]>([])
-
-  const getConvs = async() => {
-    const q = query(collection(dbService, "faqs"), orderBy("createdAt", "desc"))
-    onSnapshot(q, (snapshot) => {
-      setConvs(snapshot.docs.map(doc => ({ ...(doc.data() as Conversation), id: doc.id })))
-    })
-  }
-
-  useEffect(() => {
-    getConvs()
-  }, [])
 
   const navigate = useNavigate()
   const onLogOutClick = () => {
@@ -28,16 +16,8 @@ const Home = ({user}: {user: User}) => {
 
   return (
     <div className="App">
-      <header>
-        <h1>My FAQ</h1>
-      </header>
-      <section>
-        <header></header>
-        <article>
-          { convs.map(c => <div key={c.id}>{c.text}</div>) }
-        </article>
-      </section>
       <FaqFactory user={user} />
+      <Faqs user={user} />
       <footer>
         <p>ⓒ 2022 My FAQ</p>
         <Button size="sm" onClick={onLogOutClick}>Log Out</Button>
